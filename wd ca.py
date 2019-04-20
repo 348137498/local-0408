c2273c7b6b1975c209ff14fef1f2c24c67b7b37d
Tang <348137498@qq.com>
2019-04-20 17:39:47 +0800

    no

---
+++ wd ca.py
@@ -0,0 +1,20 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("helloworld")
+helloworld
+>>> HELLOWORLD
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    HELLOWORLD
+NameError: name 'HELLOWORLD' is not defined
+>>> (WOEEF)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    (WOEEF)
+NameError: name 'WOEEF' is not defined
+>>> ("FEF")
+'FEF'
+>>> "fefef"
+'fefef'
+>>> 
