0d1aac9279ffff44f37bf90b50c5f014d4c7b29f
Tang <348137498@qq.com>
2019-04-29 17:51:39 +0800

    0429

---
+++ 函数定义调用0429.py
@@ -0,0 +1,224 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> f(x)=x*2
+SyntaxError: can't assign to function call
+>>> x=2
+>>> f(x)=x*2
+SyntaxError: can't assign to function call
+>>> f(x)=x*2
+SyntaxError: can't assign to function call
+>>> def f(x)=+
+SyntaxError: invalid syntax
+>>> def f(x):
+	return x*2
+	f(2)
+
+	
+>>> result = f(2)
+>>> print (result)
+4
+>>> 
+>>> def d(x):
+	return x*5+1
+	d(2)
+	mm=d(3)
+	print(mm)
+
+	
+>>> 
+>>> mm=(d2)
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    mm=(d2)
+NameError: name 'd2' is not defined
+>>> def d(x):
+	return x*2+1
+	d(3)
+	mm=d(3)
+	print(mm)
+
+	
+>>> 
+>>> 
+>>> d
+<function d at 0x1033e3158>
+>>> 
+>>> def d(x):
+	return x*2+1
+	d(3)
+	result=d(3)
+	print(result)
+
+	
+>>> 
+>>> def d(x):
+	return x*2+1
+	d(3)
+result=d(3)
+print(result)
+SyntaxError: invalid syntax
+>>> def d(f):
+	return f*3+1
+	d(2)
+mm = d(2)
+SyntaxError: invalid syntax
+>>> def d(f):
+	return f*3+1
+	d(2)
+mm = d(2)
+SyntaxError: invalid syntax
+>>> 
+>>> def d(x):
+	return x*2
+	d(3)
+
+	
+>>> mm = (3)
+>>> print(mm)
+3
+>>> def d(e):
+	return e*3+1
+	d(3)
+
+	
+>>> mm = d(3)
+>>> print(mm)
+10
+>>> def m(s):
+	return s*2+(s-1)
+m(2)
+SyntaxError: invalid syntax
+>>> def m(s):
+	return s*2+(s-1)
+	m(3)
+	bn = m(3)
+	print(bn)
+
+	
+>>> 
+>>> 
+>>>  def m(s):
+	return s*2+(s-1)
+	m(3)
+	
+SyntaxError: unexpected indent
+>>>  def m(s):
+	return s*2+(s-1)
+	m(3)
+	
+SyntaxError: unexpected indent
+>>>  def m(s):
+	return s*2+(s-1)
+m(3)
+SyntaxError: unexpected indent
+>>> def o(q):
+	return o*2+(o-1)
+	o(4)
+
+	
+>>> jk=o(4)
+Traceback (most recent call last):
+  File "<pyshell#72>", line 1, in <module>
+    jk=o(4)
+  File "<pyshell#71>", line 2, in o
+    return o*2+(o-1)
+TypeError: unsupported operand type(s) for *: 'function' and 'int'
+>>> def o(q):
+	return o*2
+	o(4)
+
+	
+>>> jk = o(4)
+Traceback (most recent call last):
+  File "<pyshell#75>", line 1, in <module>
+    jk = o(4)
+  File "<pyshell#74>", line 2, in o
+    return o*2
+TypeError: unsupported operand type(s) for *: 'function' and 'int'
+>>> def o(c):
+	return o*4
+	o(5)
+
+	
+>>> jk=o(5)
+Traceback (most recent call last):
+  File "<pyshell#80>", line 1, in <module>
+    jk=o(5)
+  File "<pyshell#79>", line 2, in o
+    return o*4
+TypeError: unsupported operand type(s) for *: 'function' and 'int'
+>>> def d(x):
+	return x*2+1
+	d(3)
+	mm=d(3)
+	print(mm)
+	def d(x):
+		return x*2+1
+		d(3)
+		mm=d(3)
+		print(mm)
+
+		
+>>> 
+>>> def o(m):
+	return m*2+(m-2)
+	o(3)
+
+	
+>>> jk=o(3)
+>>> print(jk)
+7
+>>> def o(m):
+	return m*2+(m-2)
+	
+
+	
+>>> jk=o(3)
+>>> print(jk)
+SyntaxError: invalid syntax
+>>> def o(m):
+	retunr m*2+(m-2)
+	
+SyntaxError: invalid syntax
+>>> def o(m):
+	return m*2+(m-2)
+
+>>> jk=o(3)
+>>> print(jk)
+7
+>>> def o(m):
+	return m*2+(m-2)
+	print((o3))
+
+	
+>>> 
+>>> print(o(3))
+7
+>>> def k():
+	return 1+1
+
+>>> ojbk=k()
+>>> print(ojbk)
+2
+>>> >> def k():
+	return 1+1
+
+>>> ojbk=k(3)
+>>> print(ojbk)
+SyntaxError: invalid syntax
+>>> len(dfdfd)
+Traceback (most recent call last):
+  File "<pyshell#110>", line 1, in <module>
+    len(dfdfd)
+NameError: name 'dfdfd' is not defined
+>>> len('scfdv')
+5
+>>> str(ooo)
+Traceback (most recent call last):
+  File "<pyshell#112>", line 1, in <module>
+    str(ooo)
+NameError: name 'ooo' is not defined
+>>> str(100)
+'100'
+>>> 
